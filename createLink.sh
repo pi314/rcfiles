2ff4e1d610ed9b73ef97009a81d1fd7e4ccf6342
池昌言 9917070 <pi314@bsd1.nba.nctu.edu.tw>
2013-04-28 22:29:08 +0800

    修改 createLink.sh, 使其產生相對路徑的 link

--- createLink.sh
+++ createLink.sh
@@ -1,3 +1,3 @@
-ln -s ~/.rcfiles/.tcshrc ~/.tcshrc
-ln -s ~/.rcfiles/.vimrc ~/.vimrc
-ln -s ~/.rcfiles/.screenrc ~/.screenrc
+ln -s .rcfiles/.tcshrc ../.tcshrc
+ln -s .rcfiles/.vimrc ../.vimrc
+ln -s .rcfiles/.screenrc ../.screenrc
