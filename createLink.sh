0775518515a2f2cd67e6b46e040851877d54c44e
Cychih <michael66230@gmail.com>
2014-01-27 00:10:35 +0800

    Add .bashrc process into createLink.sh

--- createLink.sh
+++ createLink.sh
@@ -1,3 +1,4 @@
-ln -s .rcfiles/.tcshrc ../.tcshrc
-ln -s .rcfiles/.vimrc ../.vimrc
-ln -s .rcfiles/.screenrc ../.screenrc
+ln -s ~/.rcfiles/.tcshrc   ~/.tcshrc
+ln -s ~/.rcfiles/.vimrc    ~/.vimrc
+ln -s ~/.rcfiles/.screenrc ~/.screenrc
+ln -s ~/.rcfiles/.bashrc   ~/.bashrc
