3077934a4ef9ffcc2404c031ab84d605afc870d3
Cychih <michael66230@gmail.com>
2014-06-12 14:00:28 +0800

    More test

--- two_line_prompt.sh
+++ two_line_prompt.sh
@@ -1,23 +1,32 @@
-#!/usr/bin/env sh
-
 #[{pwd}][{window_name} {window_index}]({branch_name})
 #{time}{username}@{hostname}> {cursor here}
 
 printf "\n\n\n"
 printf "\033[1;30m|\033[1;32m[`pwd`]\033[m\n"
 printf "\033[1;30m|\033[1;36m20:38\033[1;33mpi314\033[m"
-printf "@\033[1;37mcychih-PC> \n"
+printf "@\033[1;37mcychih-PC\033[1;30m[tcsh]\033[m> \n"
 
 printf "\n\n\n"
 printf "\033[1;31m|\033[1;32m[`pwd`]\033[m(master)\033[1;34mj:3\n"
 printf "\033[1;31m|\033[1;36m20:38\033[1;33mroot\033[m"
-printf "@\033[1;37mcychih-PC\033[1;35m[screen W1]\033[m# \n"
+printf "@\033[1;37mcychih-PC\033[1;30m[tcsh]\033[1;35m[screen W1]\033[m# \n"
 
 printf "\n\n\n"
 printf "\033[1;30m=\033[1;31m=\033[1;32m=\033[1;33m=\033[1;34m="
-printf "\033[1;35m=\033[1;36m=\033[1;37m="
+printf "\033[1;35m=\033[1;36m=\033[1;37m=\033[m"
 
 printf "\n\n\n"
+
+printf "|[/home/cychih/.rcfiles]\n"
+printf "|20:38pi314@cychih-PC[`echo $SHELL | rev | cut -d'/' -f1 | rev`]> \n"
+printf "\n\n"
+
+#alias update_git_repo_flag "set git_repo_flag=${git_repo_flag}a"
+
+alias precmd 'set prompt="|[%~]`date`\n|%T%n@%m> "'
+
+#set prompt="%{^[[1;36m%}%T%{^[[m%}%{^[[1;33m%}%n%{^[[m%}@%{^[[1;37m%}%m%{^[[1;32m%}[%~]%{^[[m%}%{^[[1;35m%}[$session_name W$WINDOW]%{^[[m%}> "
+
 exit
 
 needed info
