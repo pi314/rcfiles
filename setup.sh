f70583883ebcd53a5979ec5899671b61a5bd4e7f
Cychih <michael66230@gmail.com>
2014-06-21 22:07:07 +0800

    Rearrange resource files, write setup.sh with backup feature

---
+++ setup.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+good_tag="\033[1;32m"
+not_sure_tag="\033[1;33m"
+error_tag="\033[1;31m"
+end_tag="\033[m"
+
+files="zsh/zshrc
+zsh/zshenv
+vimrc
+screenrc
+tcshrc
+bashrc
+gitconfig"
+
+for f in $files; do
+
+    if [ "${f##*/}" == $f ]; then   # normal file
+        filename="$f"
+    else                            # this file is in deeper directory
+        filename="${f##*/}"
+    fi
+
+    printf "Installing .$filename into $HOME\n"
+
+    if [ -f "$HOME/.$filename" ]; then
+
+        printf "${error_tag}$HOME/.$filename exists, do backup${end_tag}\n"
+
+        if [ ! -d "$HOME/.old_rcfiles" ]; then
+
+            printf "${error_tag}$HOME/.old_rcfiles/ directory not exist, create one${end_tag}\n"
+
+            mkdir "$HOME/.old_rcfiles"
+
+            printf "${good_tag}$HOME/.old_rcfiles ready${end_tag}\n"
+
+        fi
+
+        if [ "$BACKUP_DIR" == "" ]; then
+
+            BACKUP_DIR=`date "+%Y%m%d_%H%M%S"`
+            printf "${not_sure_tag}Create new backup directory: $HOME/.old_rcfiles/$BACKUP_DIR${end_tag}\n"
+            mkdir "$HOME/.old_rcfiles/$BACKUP_DIR"
+
+        fi
+
+        mv "$HOME/.$filename" "$HOME/.old_rcfiles/$BACKUP_DIR"
+
+        printf "${good_tag}Backup $HOME/.$filename done${end_tag}\n"
+    
+    fi
+
+    ln -s "$HOME/.rcfiles/$f" "$HOME/.$filename"
+
+    echo "Install $HOME/.$filename done"
+    echo ""
+
+done
+
+echo "Done"
+
+if [ ! "$BACKUP_DIR" == "" ]; then
+    echo "Old configuration files are now in $HOME/.old_rcfiles/$BACKUP_DIR"
+fi
