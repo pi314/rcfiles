814601b1700c3f5e5f4f72f073cc16e8f227e6cf
Cychih <michael66230@gmail.com>
2015-04-24 09:51:47 +0800

    [zsh] move ``up`` completion function into zsh/completions/_up

--- zsh/functions/up.sh
+++ zsh/functions/up.sh
@@ -1,15 +1,3 @@
-_get_parent_folders () {
-    local pwd_str="$(pwd)"
-    local climber="$(dirname $pwd_str)"
-    local indicator="../"
-    while [ "$climber" != "/" ]; do
-        echo "${indicator}:\"${climber}\" "
-        climber="$(dirname $climber)"
-        indicator="${indicator}../"
-    done
-    echo "${indicator}:\"${climber}\""
-}
-
 up () {
     if [[ $# -eq 0 ]]; then
         cd ..
