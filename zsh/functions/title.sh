662256701b477ef81d519ca3b591925ead939746
Cychih <michael66230@gmail.com>
2015-11-01 05:58:53 +0800

    [zsh][function] refine ``title`` to support tmux window title

--- zsh/functions/title.sh
+++ zsh/functions/title.sh
@@ -1,4 +1,9 @@
 title () {
+    if [ -n "$TMUX" ]; then
+        tmux rename-window "$*"
+        return
+    fi
+
     case $ZSH_KERNEL_TYPE in
         "CYGWIN")
             printf "\033]0;$*\007"
