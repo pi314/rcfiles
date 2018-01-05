d7c114e9e3d8e07274ff1a9359c8fa3a9767d217
Cychih <michael66230@gmail.com>
2018-01-05 23:25:39 +0800

    [zsh] upgrade upto()

--- zsh/functions/upto.sh
+++ zsh/functions/upto.sh
@@ -4,15 +4,19 @@ upto () {
         return 1
     fi
 
-    probe="$(dirname "$(pwd)")"
-    while [ "$(basename "${probe}")" != "$1" ] && [ "${probe}" != '/' ]; do
+    probe="$(pwd)"
+    while [ $# -gt 0 ]; do
         probe="$(dirname "${probe}")"
-    done
+        while [ "$(basename "${probe}")" != "$1" ] && [ "${probe}" != '/' ]; do
+            probe="$(dirname "${probe}")"
+        done
 
-    if [ "${probe}" = '/' ] && [ "$1" != '/' ]; then
-        echo 'Directory not found ಠ_ಠ'
-        return 1
-    fi
+        if [ "${probe}" = '/' ] && [ "$1" != '/' ]; then
+            echo 'Directory not found ಠ_ಠ'
+            return 1
+        fi
+        shift
+    done
 
     cd "${probe}"
 }
