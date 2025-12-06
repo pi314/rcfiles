a86bfa7f5fdc3fc1087167b62a122851a0adc2a6
Cychih <michael66230@gmail.com>
2025-12-06 19:14:37 +0800

    [functions/mkcd] Fix: Really always use mkdir '-p'; Refine verbose

--- functions/mkcd.sh
+++ functions/mkcd.sh
@@ -1,11 +1,13 @@
 mkcd () {
+    dollar="\033[38;5;135m$\033[m"
+
     arg_verbose=''
     arg_path=''
 
     while [ $# -ne 0 ]; do
         if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
             echo 'Usage:' >&2
-            echo '  mkcd [-p|--parents] [-v|--verbose] DIRECTORY' >&2
+            echo "${dollar}"' mkcd [-v|--verbose] DIRECTORY' >&2
             return 1
 
         elif [ "$1" = '-v' ] || [ "$1" = '--verbose' ]; then
@@ -28,17 +30,17 @@ mkcd () {
     fi
 
     if [ -n "$arg_verbose" ]; then
-        echo -e '\033[1;30m$\033[m' mkdir $arg_verbose "$arg_path"
+        echo "${dollar}" mkdir $arg_verbose "$arg_path"
     fi
 
-    mkdir $arg_verbose "$arg_path"
+    mkdir $arg_verbose -p "$arg_path"
 
     if [ ! -d "$arg_path" ]; then
         return 1
     fi
 
     if [ -n "$arg_verbose" ]; then
-        echo -e '\033[1;30m$\033[m' cd "$arg_path"
+        echo "${dollar}" cd "$arg_path"
     fi
 
     cd "$arg_path"
