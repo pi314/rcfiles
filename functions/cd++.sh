4acc0746848cb94b6379b8c98d26dbc1ef3a77bc
Cychih <michael66230@gmail.com>
2026-01-15 02:19:45 +0800

    [functions/cd--/cd++] Refine

--- functions/cd++.sh
+++ functions/cd++.sh
@@ -1,9 +1,11 @@
 cd++ () {
     if [ -z "$CWD_PROBE" ]; then
+        echo 'CWD_PROBE is empty'
         return 1
     fi
 
     if [ -z "$PWD" ]; then
+        echo 'PWD is empty'
         return 1
     fi
 
@@ -25,6 +27,7 @@ cd++ () {
             echo "$PWD\033[38;5;135m${CWD_PROBE:${#PWD}}\033[m"
             ;;
         *)
+            echo 'CWD_PROBE does not under PWD'
             return 1
             ;;
     esac
--- functions/cd--.sh
+++ functions/cd--.sh
@@ -1,11 +1,23 @@
 cd-- () {
+    if [ -z "$PWD" ]; then
+        echo 'PWD is empty'
+        return 1
+    fi
+
     if [ -z "$CWD_PROBE" ]; then
         CWD_PROBE="$PWD"
     fi
 
-    if [ -z "$PWD" ]; then
-        return 1
+    if [ "$PWD" = '/' ]; then
+        CWD="/"
+    else
+        CWD="$PWD/"
     fi
+    case "$CWD_PROBE/" in
+        "$CWD"*) ;;
+        "$CWD") ;;
+        *)  CWD_PROBE="$PWD";;
+    esac
 
     cd ..
 
@@ -19,9 +31,8 @@ cd-- () {
         "$CWD"*)
             echo "$PWD\033[38;5;135m${CWD_PROBE:${#PWD}}\033[m"
                 ;;
-        *)
-            CWD_PROBE="$OLDPWD"
-            ;;
+        *)  echo Path incorrect
+            return 1 ;;
     esac
 
     export CWD_PROBE
