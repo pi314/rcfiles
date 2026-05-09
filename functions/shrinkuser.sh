2033271fca4ef69c0514533874f45d5a7fef80b0
Cychih <michael66230@gmail.com>
2026-05-10 02:26:51 +0800

    [functions/shrinkuser] Accept stdin

--- functions/shrinkuser.sh
+++ functions/shrinkuser.sh
@@ -1,8 +1,20 @@
 shrinkuser () {
-    if ! startswith "$1" "$HOME"; then
-        echo "$1"
+    if [ -n "$1" ]; then
+        where="$1"
+    elif [ ! -t 0 ]; then
+        read where
+    else
+        where=''
+    fi
+
+    if [ -z "${where}" ]; then
         return 1
     fi
 
-    echo "~${1:${#HOME}}"
+    if ! startswith "$where" "$HOME"; then
+        echo "$where"
+        return 1
+    fi
+
+    echo "~${where:${#HOME}}"
 }
